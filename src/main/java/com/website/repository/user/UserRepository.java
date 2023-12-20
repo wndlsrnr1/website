90795cc3b8d9af680609ecf9a51a999017912701
wndlsrnr1 <76830841+wndlsrnr1@users.noreply.github.com>
2023-12-20 11:14:11 +0900

    Revert "프론트에 따라 몇가지 수정"

--- src/main/java/com/website/repository/user/UserRepository.java
+++ src/main/java/com/website/repository/user/UserRepository.java
@@ -1,55 +1,7 @@
 package com.website.repository.user;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.website.domain.user.User;
-import org.springframework.stereotype.Repository;
+import com.website.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import static com.website.domain.user.QUser.*;
-
-import javax.persistence.EntityManager;
-import java.util.List;
-
-@Repository
-public class UserRepository {
-
-    private final EntityManager entityManager;
-    private final JPAQueryFactory jpaQueryFactory;
-    private final UserJpaRepository userJpaRepository;
-
-    public UserRepository(EntityManager entityManager, UserJpaRepository userJpaRepository) {
-        this.entityManager = entityManager;
-        this.jpaQueryFactory = new JPAQueryFactory(entityManager);
-        this.userJpaRepository = userJpaRepository;
-    }
-
-    public User findNormalUserByEmailPassword(String emailParam, String passwordParam) {
-
-        return jpaQueryFactory.select(user)
-                .from(user)
-                .where(user.email.eq(emailParam), user.password.eq(passwordParam))
-                .fetchOne();
-    }
-
-    public User findByUserId(Long userId) {
-        return userJpaRepository.findById(userId).get();
-    }
-
-
-    public void saveUser(User user) {
-        userJpaRepository.save(user);
-    }
-
-    public User findUserByEmail(String email) {
-        List<User> fetch = jpaQueryFactory.selectFrom(user).where(user.email.eq(email)).fetch();
-        if (fetch.size() == 0) {
-            return null;
-        }
-        return fetch.get(0);
-    }
-
-
-    //public User fail() {
-    //    List select_noname_from_user = entityManager.createQuery("select name from user").getResultList();
-    //    return null;
-    //}
+public interface UserRepository extends JpaRepository<User, Long> {
 }
