e6f68d85235784a8262342cf7bc34e812b4c0131
ik <wndlsrnr1@gmail.com>
2023-12-21 20:15:52 +0900

    서브카테고리 CRUD
    controller, repository, service

--- src/main/java/com/website/web/service/user/LoginService.java
+++ src/main/java/com/website/web/service/user/LoginService.java
@@ -7,6 +7,12 @@ import com.website.web.dto.request.user.LoginFormRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import com.website.web.constance.Regexes;
+import com.website.web.dto.common.ApiError;
+import com.website.web.dto.common.ApiResponseBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +20,7 @@ import org.springframework.stereotype.Service;
 public class LoginService {
 
     private final UserRepository userRepository;
+    private final LoginFormValidatorEx loginFormValidatorEx;
 
     public User join(JoinFormRequest joinFormRequest) {
 
@@ -23,8 +30,33 @@ public class LoginService {
                 .password(joinFormRequest.getPassword())
                 .build();
         userRepository.saveUser(user);
-
         return user;
+    }
+
+
+
+    //public User findUser(LoginFormRequest loginFormRequest) {
+    //    String password = loginFormRequest.getPassword();
+    //    String email = loginFormRequest.getEmail();
+    //
+    //    User findNormalUser = userJpaRepository.findNormalUserByEmailPassword(email, password);
+    //
+    //    if (findNormalUser == null) {
+    //        return null;
+    //    }
+    //
+    //    return findNormalUser;
+    //}
+    public User findUser(LoginFormRequest loginFormRequest) {
+        String password = loginFormRequest.getPassword();
+        String email = loginFormRequest.getEmail();
+
+        User findNormalUser = userRepository.findNormalUserByEmailPassword(email, password);
+
+        if (findNormalUser == null) {
+            return null;
+        }
+        return findNormalUser;
     }
 
     //public User findUser(LoginFormRequest loginFormRequest) {
@@ -40,18 +72,66 @@ public class LoginService {
     //    return findNormalUser;
     //}
 
-    public User findUser(LoginFormRequest loginFormRequest) {
-        String password = loginFormRequest.getPassword();
-        String email = loginFormRequest.getEmail();
 
-        User findNormalUser = userRepository.fail();
 
-        if (findNormalUser == null) {
-            return null;
-        }
 
-        return findNormalUser;
+    public User findUserByUserId(Long userId) {
+        return userRepository.findByUserId(userId);
     }
 
 
+    public ResponseEntity validateEmail(String email) {
+        if (email == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        boolean matches = email.matches(Regexes.EMAIL_PATTERN);
+        log.info("matches = {}", matches);
+        //email형식이 맞지 않으면 badReqeust Return
+        if (!matches) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        //User가 있으면 문제 있음
+        User findUser = userRepository.findUserByEmail(email);
+        if (findUser != null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        //확실히 하기위해서 조건문
+        if (matches && findUser == null) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+
+
+    }
+
+    public ResponseEntity joinUser(JoinFormRequest joinFormRequest, BindingResult bindingResult) {
+
+        log.info("joinFormRequest = {}", joinFormRequest);
+
+        loginFormValidatorEx.validateEachPasswordEquals(joinFormRequest, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            ApiResponseBody body = ApiResponseBody.builder()
+                    .data(null)
+                    .apiError(new ApiError(bindingResult)).build();
+            return ResponseEntity.badRequest().body(body);
+        }
+
+        //DB 조회 후 중복 방지
+        loginFormValidatorEx.validateDuplicatedEmail(joinFormRequest, bindingResult);
+        if (bindingResult.hasErrors()) {
+            ApiResponseBody body = ApiResponseBody.builder()
+                    .data(null)
+                    .apiError(new ApiError(bindingResult)).build();
+            return ResponseEntity.badRequest().body(body);
+        }
+
+        //정상 처리
+        User joinedUser = this.join(joinFormRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
