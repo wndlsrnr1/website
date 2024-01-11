66ccb77d0d93d60d1696f19ed3c0a30c05666743
ik <wndlsrnr1@gmail.com>
2024-01-11 19:21:30 +0900

    delete unnecessary java file

--- src/main/java/com/website/repository/category/SubcategoryJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.website.repository.category;
-
-import com.website.domain.category.Subcategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubcategoryJpaRepository extends JpaRepository<Subcategory, Long> {
-
-}
--- src/main/java/com/website/repository/item/ItemJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.website.repository.item;
-
-import com.website.domain.item.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemJpaRepository extends JpaRepository<Item, Long> {
-}
