9ac75c83994a3ca285780b103a8017173f3f7a3b
ik <wndlsrnr1@gmail.com>
2024-01-30 17:29:16 +0900

    Item Upload V2

--- src/test/java/com/website/web/service/attachment/FileServiceTest.java
+++ src/test/java/com/website/web/service/attachment/FileServiceTest.java
@@ -3,6 +3,8 @@ package com.website.web.service.attachment;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -13,6 +15,12 @@ class FileServiceTest {
         String s = "sadfasdf.asdfasdfasf";
         String substring = s.substring(0, 8);
         log.info(substring);
+    }
+
+    @Test
+    public void LDT() {
+        String s = LocalDateTime.now().toString();
+        System.out.println("s = " + s);
     }
 
 }
