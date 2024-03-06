b04fe5dc2438efea6cb1edcda50bc2c28c002643
ik <wndlsrnr1@gmail.com>
2024-03-06 19:24:13 +0900

    add item home carousel method / 03 / 06 addMethod 완성. priority 가장 마지막 값으로 선택함. carouselList 조건 출력.

--- src/main/java/com/website/web/controller/api/admin/item/home/carousel/HomeCarouselManagementController.java
+++ src/main/java/com/website/web/controller/api/admin/item/home/carousel/HomeCarouselManagementController.java
@@ -1,16 +1,19 @@
 package com.website.web.controller.api.admin.item.home.carousel;
 
-import com.website.repository.item.carousel.ItemHomeCarouselRepository;
 import com.website.web.dto.request.item.carousel.CarouselAddRequest;
+import com.website.web.dto.request.item.carousel.CarouselSearchCond;
 import com.website.web.service.item.carousel.ItemHomeCarouselService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @Slf4j
 @RestController
@@ -24,6 +27,11 @@ public class HomeCarouselManagementController {
     @PostMapping("/home/carousel/add")
     public ResponseEntity addHomeCarousel(@Validated CarouselAddRequest carouselAddRequest, BindingResult bindingResult) {
         return itemHomeCarouselService.addCarousel(carouselAddRequest, bindingResult);
+    }
+
+    @GetMapping("/home/carousels")
+    public ResponseEntity requestHomeCarouselList(@Validated CarouselSearchCond carouselSearchCond, BindingResult bindingResult, Pageable pageable) {
+        return itemHomeCarouselService.getCarousels(carouselSearchCond, bindingResult, pageable);
     }
 
 }
