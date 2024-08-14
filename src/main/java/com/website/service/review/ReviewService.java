f1b74f36ffed0ef5bf8c6295d1d0b2b67dd684c3
ik <wndlsrnr1@gmail.com>
2024-08-14 20:43:16 +0900

    [#46] implement create, read, update, delete method in ReviewService layer. implement testing method for ReviewService.registerReview.
    not implemented paging Review in ReviewService
    todo: implement paging Review
    todo: implement testing method of lest of it.

--- src/main/java/com/website/service/review/ReviewService.java
+++ src/main/java/com/website/service/review/ReviewService.java
@@ -1,8 +1,102 @@
 package com.website.service.review;
 
+import com.website.exception.ClientException;
+import com.website.exception.ErrorCode;
+import com.website.repository.item.ItemRepository;
+import com.website.repository.model.item.Item;
+import com.website.repository.model.user.User;
+import com.website.repository.review.ReviewRepository;
+import com.website.repository.review.model.Review;
+import com.website.repository.user.UserRepository;
+import com.website.service.item.ItemValidator;
+import com.website.service.review.model.ReviewCreateDto;
+import com.website.service.review.model.ReviewDto;
+import com.website.service.review.model.ReviewRequestDto;
+import com.website.service.review.model.ReviewUpdateDto;
+import com.website.service.user.UserValidator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class ReviewService {
 
+    private final ReviewRepository reviewRepository;
+    private final UserValidator userValidator;
+    private final ItemValidator itemValidator;
+
+    // create
+    @Transactional
+    public ReviewDto registerReview(ReviewCreateDto dto) {
+        User user = userValidator.validateAndGet(dto.getUserId());
+        Item item = itemValidator.validateAndGet(dto.getItemId());
+
+        Review review = Review.builder()
+                .item(item)
+                .user(user)
+                .star(dto.getStar())
+                .content(dto.getContent())
+                .build();
+
+        Review savedReview = reviewRepository.save(review);
+        return ReviewDto.of(savedReview);
+    }
+
+    // read
+    @Transactional(readOnly = true)
+    public ReviewDto getReviewById(Long reviewId) {
+        if (reviewId == null) {
+            throw new ClientException(ErrorCode.BAD_REQUEST, "reviewId is null");
+        }
+
+        Review foundReview = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ClientException(ErrorCode.BAD_REQUEST, "review not found. reviewId = " + reviewId));
+
+        return ReviewDto.of(foundReview);
+    }
+
+    @Transactional(readOnly = true)
+    public ReviewDto getReviewByUserIdAndItemId(Long userId, Long itemId) {
+        User user = userValidator.validateAndGet(userId);
+        Item item = itemValidator.validateAndGet(itemId);
+
+        Review foundReview = reviewRepository.findByUserAndItem(user, item).orElseThrow(() ->
+                new ClientException(ErrorCode.BAD_REQUEST, "review not found. userId = " + userId + ", itemId = " + itemId));
+
+        return ReviewDto.of(foundReview);
+    }
+
+    // update
+    @Transactional
+    public ReviewDto updateReview(ReviewUpdateDto dto) {
+        if (dto.getId() == null) {
+            throw new ClientException(ErrorCode.BAD_REQUEST, "reviewId is null. review = " + dto);
+        }
+
+        Review review = reviewRepository.findById(dto.getId()).orElseThrow(
+                () -> new ClientException(ErrorCode.BAD_REQUEST, "review not found reviewId = " + dto.getId())
+        );
+
+        review.setContent(dto.getContent());
+        review.setStar(dto.getStar());
+        Review updatedReview = reviewRepository.save(review);
+        return ReviewDto.of(updatedReview);
+    }
+
+    //delete
+    @Transactional
+    public void removeReview(Long userId, Long itemId) {
+        User user = userValidator.validateAndGet(userId);
+        Item item = itemValidator.validateAndGet(itemId);
+
+        Review foundReview = reviewRepository.findByUserAndItem(user, item).orElseThrow(() ->
+                new ClientException(ErrorCode.BAD_REQUEST, "review not found. userId = " + userId + ", itemId = " + itemId));
+
+        reviewRepository.delete(foundReview);
+    }
 }
--- src/main/java/com/website/service/review/model/ReviewDto.java
+++ src/main/java/com/website/service/review/model/ReviewDto.java
@@ -1,4 +1,34 @@
 package com.website.service.review.model;
 
+import com.website.repository.review.model.Review;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReviewDto {
+    private Long id;
+    private Long itemId;
+    private Long userId;
+    private Integer star;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static ReviewDto of(Review savedReview) {
+        return ReviewDto.builder()
+                .id(savedReview.getId())
+                .itemId(savedReview.getItem().getId())
+                .userId(savedReview.getUser().getId())
+                .star(savedReview.getStar())
+                .createdAt(savedReview.getCreatedAt())
+                .updatedAt(savedReview.getUpdatedAt())
+                .build();
+    }
 }
